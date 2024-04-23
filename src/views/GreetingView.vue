2b03d2995243cfaaa8fa9cbccda4dd079253b0fc
Skhrebtan <shrebtan@gmail.com>
2024-04-24 00:11:52 +0300

    edit title and favicon

--- src/views/GreetingView.vue
+++ src/views/GreetingView.vue
@@ -7,10 +7,10 @@ import BaseLayout from '@/layouts/BaseLayout.vue'
   <BaseLayout>
     <div class="text-center">
       <img class="inline mb-6" src="../assets/imgs/map-pin.svg" alt="" />
-      <h1 class="font-bold text-4xl mb-7">IT traveler</h1>
+      <h1 class="font-bold text-4xl mb-7">IT traveller</h1>
       <p class="leading-6 mb-11">
-        Простий і зручний веб додаток, який дозволить тобі відмічати твої улюблені місця, а також
-        ті, в яких би ти дуже хотів побувати. Тож не зволікай і спробуй сам.
+        A simple and convenient web application that allows you to bookmark your favorite places as
+        well as those in which you would really like to visit. So don't delay and try it yourself.
       </p>
       <IButton to="/auth/registration">Start exploring</IButton>
     </div>
